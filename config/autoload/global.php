8d2f6ad86678cea248691c4aecb2ee8d5e5b1b46
Christopher Vandomme <christopher.vandomme@gmail.com>
2015-12-08 15:28:25 +0100

    controleur + routes

---
+++ config/autoload/global.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+/*
+La connexion à la base de donnée ce fait dans ce fichier en PDO, il ne faut pas oublier de préciser que l'on récupère
+ou envoie tout les résultat en UTF8
+*/
+return array(
+
+ );
---
+++ module/MiniModule/config/module.config.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace MiniModule;
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'index' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'MiniModule\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'MiniModule\Controller\Index' => Controller\IndexController::class,
+        ),
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'minimodule/index/index' => __DIR__ . '/../view/minimodule/index/index.phtml',
+            'error'             => __DIR__ . '/../view/error.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    // Placeholder for console routes
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+            ),
+        ),
+    ),
+);
